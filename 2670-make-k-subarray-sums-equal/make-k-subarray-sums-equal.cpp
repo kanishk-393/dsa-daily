46e151dd99dd0d006d7b3e71daec72bebbc297f3
kanishk-393 <kanishkt393@gmail.com>
2025-04-30 20:21:21 +0530

    Time: 33 ms (63.25%) | Memory: 114.5 MB (76.92%) - LeetSync

---
+++ 2670-make-k-subarray-sums-equal/make-k-subarray-sums-equal.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    long long makeSubKSumEqual(vector<int>& nums, int k) {
+        int n=nums.size();
+        int g =gcd(n,k);
+        long long ans=0;
+        for(int i=0;i<g;i++){
+            vector<int>group;
+            int j=i;
+            while (true){
+                group.push_back(nums[j]);
+                j=(j+k)%n;
+                if(j==i){
+                    break;
+                }
+            }
+            sort(group.begin(),group.end());
+            int median=group[group.size() / 2];    
+            for(int x:group){
+            ans += llabs(1LL * x - median);
+
+            }
+                    }
+return ans;
+    }
+};
